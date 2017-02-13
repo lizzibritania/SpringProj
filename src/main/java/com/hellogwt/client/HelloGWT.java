2f49945eb0e01409f0e8596d04508123e98389d5
lizzibritania <elizbeth@li.ru>
2017-02-13 19:15:23 +0300

    Initial commit

---
+++ src/main/java/com/hellogwt/client/HelloGWT.java
@@ -0,0 +1,19 @@
+
+package com.hellogwt.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
+public class HelloGWT implements EntryPoint {
+
+    @Override
+
+    public void onModuleLoad() {
+
+        Label greetingLabel = new Label("Hello, GWT!");
+
+        RootPanel.get().add(greetingLabel);
+
+    }
+
+}
